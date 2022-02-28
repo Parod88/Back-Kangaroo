bfb7886af6e1a54be9f26b98030f8452a5d4cf37
Parod88 <pabrodgom@gmail.com>
2022-02-28 11:50:28 +0100

    GET method done

--- controllers/advertisementController.js
+++ controllers/advertisementController.js
@@ -14,6 +14,22 @@ const exampleAdvertisementMethod = async (req, res, next) => {
   }
 };
 
+//CRUD
+
+// GET/api/v1/anuncios
+const getAdvertisementsList = async (req, res, next) => {
+  try {
+    const advertisementsList = await Advertisements.find();
+    res.status(200).json({data: advertisementsList});
+  } catch (error) {
+    // res.status(500).send({
+    //   message: 'An error occurred.'
+    // });
+    next(error);
+  }
+};
+
 module.exports = {
-  exampleAdvertisementMethod
+  exampleAdvertisementMethod,
+  getAdvertisementsList
 };
--- routes/api/v1/advertisement.js
+++ routes/api/v1/advertisement.js
@@ -2,9 +2,12 @@
 
 const express = require('express');
 const router = express.Router();
-const {exampleAdvertisementMethod} = require('../../../controllers/advertisementController.js');
+const {
+  exampleAdvertisementMethod,
+  getAdvertisementsList
+} = require('../../../controllers/advertisementController.js');
 
 // Routes
-router.get('/', exampleAdvertisementMethod);
+router.get('/', getAdvertisementsList);
 
 module.exports = router;
