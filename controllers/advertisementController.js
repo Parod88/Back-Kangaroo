187847f0e2024fe4e550549ebf686e59e9563a20
Parod88 <pabrodgom@gmail.com>
2022-02-28 13:13:53 +0100

    almost finished task

--- controllers/advertisementController.js
+++ controllers/advertisementController.js
@@ -2,25 +2,14 @@
 
 const AdvertisementModel = require('../models/Advertisement.js');
 
-// const exampleAdvertisementMethod = async (req, res, next) => {
-//   try {
-//     const advertisements = await AdvertisementModel.find({});
-//     res.status(200).json({data: advertisements});
-//   } catch (error) {
-//     res.status(500).send({
-//       message: 'An error occurred.'
-//     });
-//     next(err);
-//   }
-// };
-
 //CRUD
 
 // GET/api/v1/anuncios
 const getAdvertisementsList = async (req, res, next) => {
   try {
     const advertisementsList = await AdvertisementModel.find().sort({updatedAt: -1});
-    res.status(200).json({data: advertisementsList});
+    // res.status(200).json({data: advertisementsList});
+    res.status(302).redirect('/api/v1/advertisements/1');
   } catch (error) {
     // res.status(500).send({
     //   message: 'An error occurred.'
--- routes/api/v1/advertisement.js
+++ routes/api/v1/advertisement.js
@@ -3,7 +3,6 @@
 const express = require('express');
 const router = express.Router();
 const {
-  exampleAdvertisementMethod,
   getAdvertisementsList,
   getPaginatedAdvertisementsList
 } = require('../../../controllers/advertisementController.js');
