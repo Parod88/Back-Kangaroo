773f003fb7d7c09b4f7cabd33654ba5ce0db9d1b
Parod88 <pabrodgom@gmail.com>
2022-02-28 12:07:07 +0100

    advertisement controller and route on app changes

--- controllers/advertisementController.js
+++ controllers/advertisementController.js
@@ -2,24 +2,24 @@
 
 const AdvertisementModel = require('../models/Advertisement.js');
 
-const exampleAdvertisementMethod = async (req, res, next) => {
-  try {
-    const advertisements = await AdvertisementModel.find({});
-    res.status(200).json({data: advertisements});
-  } catch (error) {
-    res.status(500).send({
-      message: 'An error occurred.'
-    });
-    next(err);
-  }
-};
+// const exampleAdvertisementMethod = async (req, res, next) => {
+//   try {
+//     const advertisements = await AdvertisementModel.find({});
+//     res.status(200).json({data: advertisements});
+//   } catch (error) {
+//     res.status(500).send({
+//       message: 'An error occurred.'
+//     });
+//     next(err);
+//   }
+// };
 
 //CRUD
 
 // GET/api/v1/anuncios
 const getAdvertisementsList = async (req, res, next) => {
   try {
-    const advertisementsList = await Advertisements.find();
+    const advertisementsList = await AdvertisementModel.find();
     res.status(200).json({data: advertisementsList});
   } catch (error) {
     // res.status(500).send({
@@ -30,6 +30,5 @@ const getAdvertisementsList = async (req, res, next) => {
 };
 
 module.exports = {
-  exampleAdvertisementMethod,
   getAdvertisementsList
 };
