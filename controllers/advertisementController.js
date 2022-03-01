8ab70a07b9efb1ff1eeb41fa6c9eb5973d988d19
Parod88 <pabrodgom@gmail.com>
2022-03-01 19:49:51 +0100

    get list and get paginated list, both sorted by updatedDate

--- controllers/advertisementController.js
+++ controllers/advertisementController.js
@@ -4,16 +4,16 @@ const AdvertisementModel = require('../models/Advertisement.js');
 
 //CRUD
 
-// GET/api/v1/anuncios
+// GET/api/v1/advertisements
 const getAdvertisementsList = async (req, res, next) => {
   try {
     const advertisementsList = await AdvertisementModel.find().sort({updatedAt: -1});
-    // res.status(200).json({data: advertisementsList});
-    res.status(302).redirect('/api/v1/advertisements/1');
+    res.status(200).json({data: advertisementsList});
+    // res.status(302).redirect('/api/v1/advertisements/1');
   } catch (error) {
-    // res.status(500).send({
-    //   message: 'An error occurred.'
-    // });
+    res.status(500).send({
+      message: 'An error occurred.'
+    });
     next(error);
   }
 };
@@ -23,9 +23,9 @@ const getPaginatedAdvertisementsList = async (req, res, next) => {
   let page = req.params.page || 1;
   try {
     const paginatedAdvertisements = await AdvertisementModel.find({})
+      .sort({updatedAt: -1})
       .skip(perPage * page - perPage)
       .limit(perPage)
-      .sort({updatedAt: -1})
       .exec();
     res.json({data: paginatedAdvertisements});
   } catch (error) {
