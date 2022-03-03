1c70bfc826c1cf077710977d19268fa1b1096936
Snozin <msl.snzn@gmail.com>
2022-03-03 20:33:48 +0000

    Creada funcionalidad de registro de usuarios en backend

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const UserModel = require('../models/User.js');
+const User = require('../models/User.js');
 
 const exampleUserMethod = async (req, res, next) => {
   try {
-    const users = await UserModel.find({});
+    const users = await User.find({});
     res.status(200).json({data: users});
   } catch (error) {
     res.status(500).send({
@@ -14,6 +14,25 @@ const exampleUserMethod = async (req, res, next) => {
   }
 };
 
+const register = async (req, res, next) => {
+  try {
+    const {name, email, password, isAdmin} = req.body;
+
+    const hashedPassword = await User.hashPassword(password);
+
+    const newUser = new User({name, email, hashedPassword});
+    await newUser.save();
+
+    res.status(201).json({newUser});
+  } catch (error) {
+    res.status(418).send({
+      message: 'Error in user creation process.'
+    });
+    next(error);
+  }
+};
+
 module.exports = {
-  exampleUserMethod
+  exampleUserMethod,
+  register
 };
--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -3,8 +3,12 @@
 const express = require('express');
 const router = express.Router();
 const {exampleUserMethod} = require('../../../controllers/userController.js');
+const {register} = require('../../../controllers/userController.js');
 
 // Routes
 router.get('/', exampleUserMethod);
+
+// post -> /user/register
+router.post('/register', register);
 
 module.exports = router;
