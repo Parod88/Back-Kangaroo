af9e3f610ea81291819092859cae93c81237adce
pablopsdigital <pablopsdigital@gmail.com>
2022-03-28 14:57:49 +0200

    add producto to conversations

--- controllers/chat/conversationController.js
+++ controllers/chat/conversationController.js
@@ -3,25 +3,38 @@
 const ConversationModel = require('../../models/chat/Conversation.js');
 const AdvertisementModel = require('../../models/Advertisement.js');
 const UserModel = require('../../models/User.js');
+const {Console} = require('console');
 
 //Create a new conversation
 const createConversation = async (req, res, next) => {
+  console.log('body', req.body);
   const {userSenderId, userReceiverId, advertisementId} = req.body;
-  const advertisement = await AdvertisementModel.findOne({advertisementId});
+  // const advert = await AdvertisementModel.findById(req.body.advertisementId);
+  const conversations = await ConversationModel.find({advertisement: advertisementId});
+  console.log('conver', conversations);
 
   try {
-    // if (user) {
-    //   res.status(400).json({
-    //     message: 'Email already exists'
-    //   });
-    //   return;
-    // }
+    if (conversations.length > 0) {
+      res.status(400).json({
+        message: 'Conversation already exists'
+      });
+      return;
+    }
 
     const newConversation = new ConversationModel({
       members: [req.body.userSenderId, req.body.userReceiverId],
       advertisement: req.body.advertisementId
     });
     const saveConversation = await newConversation.save();
+
+    const userAddConversation = await UserModel.findByIdAndUpdate(
+      {_id: userSenderId},
+      {
+        conversations: saveConversation._id
+      },
+      {new: true} // Return final state
+    );
+
     res.status(200).json({results: saveConversation});
   } catch (error) {
     res.status(500).send({
@@ -33,7 +46,6 @@ const createConversation = async (req, res, next) => {
 
 //Get conversation of a user
 const getAllUserConversations = async (req, res, next) => {
-  console.log(req.body);
   try {
     const userConversations = await ConversationModel.find({members: {$in: [req.params.userId]}});
     res.status(200).json({results: userConversations});
