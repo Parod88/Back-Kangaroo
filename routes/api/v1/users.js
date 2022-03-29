31390cc0e7aa839e02d2c9e90309ba4b41accf89
Parod88 <pabrodgom@gmail.com>
2022-03-29 17:38:26 +0200

    change password

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -8,7 +8,8 @@ const {
   confirmSignUp,
   deleteUser,
   getAllUsers,
-  getOneUserForId
+  getOneUserForId,
+  changePassword
 } = require('../../../controllers/userController.js');
 const {forgotPassword, resetPassword} = require('../../../controllers/resetPasswordController');
 const User = require('../../../models/User');
@@ -23,6 +24,7 @@ router.get('/', getAllUsers); //TODO: if auth implement
 router.get('/:userId', getOneUserForId); //TODO: if auth implement
 router.post('/register', register);
 router.put('/confirm-signup/:confirmToken', confirmSignUp);
+router.put('change-password/:userId', isAuth, changePassword);
 router.delete('/:userId', isAuth, deleteUser); //TODO Volver A PONER jwt middleware
 router.put('/:userId', isAuth, updateUser);
 
